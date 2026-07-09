42ea11e12336a2d7b0bb41e3419dcba52b6f2fd7
Joshua Hodges <jjhodges20@gmail.com>
2026-07-09 09:51:20 -0600

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,24 @@
+# This Project is to create a little "About Me" that stores variables about my personal information and prints them
+# This is the first time I have written a code on my own within VS Code
+# This code is broken down into variables, then print them in the called out format.
+
+first_name = "Josh"
+last_name = "Hodges"
+age = 28
+city = "Denver"
+previous_job = "Production Planner Sr."
+reason_for_learning = "I want to learn more about coding to help grow my skill sets."
+fun_fact = "I am a big Lord of the Rings nerd, but have grown to like the movies more than the books."
+
+print("=" * 40)
+print("Student Profile")
+print("=" * 40)
+print(f"Name: {first_name} {last_name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+print(f"Previous Job: {previous_job}")
+
+print(f"Reason for Learning: {reason_for_learning}")
+
+print(f"Fun Fact: {fun_fact}")
+print("=" * 40)
